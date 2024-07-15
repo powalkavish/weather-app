9f5df901c07f646fb530bdc9224a7f00be13ab92
powalkavish <124475762+powalkavish@users.noreply.github.com>
2024-07-16 01:07:25 +0530

    Update Weather app.py

--- Weather app.py
+++ Weather app.py
@@ -1,9 +1,11 @@
-import requests
+import requests as rq
+import matplotlib 
+import ploty 
 api_key = '30d4741c779ba94c470ca1f63045390a'
 
 user_input = input("Enter city: ")
 
-weather_data = requests.get(
+weather_data = rq.get(
     f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
 
 if weather_data.json()['cod'] == '404':
